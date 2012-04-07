07823609f9739944b147a632e86550679b84ebc2
icker <apple@applematoMacBook-Pro.local>
2012-04-07 16:21:41 +0800

    Initial Commit

---
+++ woniu/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  woniu
+//
+//  Created by  on 12-4-7.
+//  Copyright __MyCompanyName__ 2012年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
---
+++ woniu/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  woniu
+//
+//  Created by  on 12-4-7.
+//  Copyright __MyCompanyName__ 2012年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+    [pool release];
+    return retVal;
+}
